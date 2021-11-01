ac51e61fd2e213f1ccbc801c8a7791b51ac78529
laribrito <lary.29.ds@gmail.com>
2021-11-01 07:08:04 -0300

    Adiciona os avisos de licença

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+"""
+This file is part of Hinário Adventista - não oficial.
+
+Hinário Adventista - não oficial is free software: you can redistribute
+it and/or modify it under the terms of the GNU General Public License 
+as published by the Free Software Foundation, either version 3 of
+the License, or any later version.
+
+Hinário Adventista - não oficial is distributed in the hope that 
+it will be useful, but WITHOUT ANY WARRANTY; without even the 
+implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Hinário Adventista - não oficial.  If not, see <https://www.gnu.org/licenses/>6.
+"""
+
 from PyQt5 import QtWidgets
 import sys
 from telaprincipal import Principal
